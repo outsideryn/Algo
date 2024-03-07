6c6600b377a3902882042949d2d1af73189fa82e
wushuang <outsideryn@gmail.com>
2024-03-07 09:34:59 +0800

    初始化

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Description:
+ * <p>
+ * date: 2024/3/7 09:32
+ *
+ * @author wushuang
+ * @since JDK 1.8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
